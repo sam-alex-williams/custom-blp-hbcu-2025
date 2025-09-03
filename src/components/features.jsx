292d1cc3e8322665daeee7be05adacd5f5828cbb
sam-alex-williams <samuel.ale.williams@gmail.com>
2025-09-03 14:34:25 -0500

    changed bg color from red to black

--- src/components/features.jsx
+++ src/components/features.jsx
@@ -19,6 +19,18 @@ export const Features = (props) => {
               ))
             : "Loading..."}
         </div>
+        <div className="row">
+          {props.data
+            ? props.data.map((d, i) => (
+                <div key={`${d.title}-${i}`} className="col-xs-6 col-md-3">
+                  {" "}
+                  <i className={d.icon}></i>
+                  <h3>{d.title}</h3>
+                  <p>{d.text}</p>
+                </div>
+              ))
+            : "Loading..."}
+        </div>
       </div>
     </div>
   );
