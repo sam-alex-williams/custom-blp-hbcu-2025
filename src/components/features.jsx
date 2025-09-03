e7b19dfb0036ba0047848df22b1a4b0d9a2a0281
sam-alex-williams <samuel.ale.williams@gmail.com>
2025-09-03 15:34:24 -0500

    updated icon color & features back to match board game theme black, red

--- src/components/features.jsx
+++ src/components/features.jsx
@@ -1,27 +1,27 @@
 import React from "react";
+import '../styles.css';
 
 export const Features = (props) => {
   return (
     <div id="features" className="text-center">
-      <div className="container">
+      <div className="container feature-container">
         <div className="col-md-10 col-md-offset-1 section-title">
           <h2>Why You'll Love It</h2>
         </div>
         <div className="row">
           {props.data
-            ? props.data.map((d, i) => (
+            ? props.data.features.map((d, i) => (
                 <div key={`${d.title}-${i}`} className="col-xs-6 col-md-3">
                   {" "}
                   <i className={d.icon}></i>
                   <h3>{d.title}</h3>
-                  <p>{d.text}</p>
                 </div>
               ))
             : "Loading..."}
         </div>
         <div className="row">
           {props.data
-            ? props.data.map((d, i) => (
+            ? props.data.traits.map((d, i) => (
                 <div key={`${d.title}-${i}`} className="col-xs-6 col-md-3">
                   {" "}
                   <i className={d.icon}></i>
