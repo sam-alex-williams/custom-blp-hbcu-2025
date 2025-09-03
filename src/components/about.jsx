e445e1c78867a27af63026461bef2a4b2ccb623a
sam-alex-williams <samuel.ale.williams@gmail.com>
2025-09-03 12:50:07 -0500

    updated content for about us

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -11,31 +11,33 @@ export const About = (props) => {
           </div>
           <div className="col-xs-12 col-md-6">
             <div className="about-text">
-              <h2>About Us</h2>
-              <p>{props.data ? props.data.paragraph : "loading..."}</p>
-              <h3>Why Choose Us?</h3>
-              <div className="list-style">
-                <div className="col-lg-6 col-sm-6 col-xs-12">
-                  <ul>
-                    {props.data
-                      ? props.data.Why.map((d, i) => (
-                          <li key={`${d}-${i}`}>{d}</li>
-                        ))
-                      : "loading"}
-                  </ul>
-                </div>
-                <div className="col-lg-6 col-sm-6 col-xs-12">
-                  <ul>
-                    {props.data
-                      ? props.data.Why2.map((d, i) => (
-                          <li key={`${d}-${i}`}> {d}</li>
-                        ))
-                      : "loading"}
-                  </ul>
-                </div>
-              </div>
+              <h2>Why Play HBCU Bingo?</h2>
+              {props.data 
+                ? props.data.paragraph.map((text, i) => 
+                  <p key={i}>{text}</p>
+                ) 
+                : <p>loading...</p>}
             </div>
           </div>
+        </div>
+        <div className="row justify-content-center">
+          <div className="col-xs-12 col-md-7">
+            <div className="about-text">
+            <h2>Why Choose Us?</h2>
+            <div>
+            <div className="list-style">
+              <ul>
+                {props.data
+                  ? props.data.Why.map((d, i) => (
+                    <li key={`${d}-${i}`}>{d}</li>
+                  ))
+                  : "loading"}
+              </ul>
+            </div>
+        </div>
+            </div>
+          </div>
+          
         </div>
       </div>
     </div>
